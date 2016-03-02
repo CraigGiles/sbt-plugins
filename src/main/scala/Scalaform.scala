d585279a8e2801b2444ff7b8000fd59ff3e5e947
Craig Giles <craig@gilesc.com>
2016-03-02 05:53:57 -0800

    Updated scalaform

--- src/main/scala/Scalaform.scala
+++ src/main/scala/Scalaform.scala
@@ -3,34 +3,27 @@ package com.gilesc
 import sbt._
 import sbt.Keys._
 import complete.DefaultParsers._
+import com.typesafe.sbt.SbtScalariform
 import com.typesafe.sbt.SbtScalariform.ScalariformKeys
+import com.typesafe.sbt.SbtMultiJvm.MultiJvmKeys.MultiJvm
 
 import scalariform.formatter.preferences._
 
 object Scalaform extends sbt.AutoPlugin {
-  lazy val scalaformSettings =
-    ScalariformKeys.preferences := ScalariformKeys.preferences.value
-      .setPreference(RewriteArrowSymbols, false)
-      .setPreference(IndentSpaces, 2)
-      .setPreference(SpaceBeforeColon, false)
-      .setPreference(CompactStringConcatenation, false)
-      .setPreference(PreserveSpaceBeforeArguments, false)
-      .setPreference(AlignParameters, false)
-      .setPreference(AlignArguments, false)
+  lazy val settings = SbtScalariform.scalariformSettings ++ Seq(
+    ScalariformKeys.preferences in Compile  := formattingPreferences,
+    ScalariformKeys.preferences in Test     := formattingPreferences,
+    ScalariformKeys.preferences in MultiJvm := formattingPreferences
+  )
+
+  def formattingPreferences = {
+    import scalariform.formatter.preferences._
+    FormattingPreferences()
       .setPreference(DoubleIndentClassDeclaration, true)
-      .setPreference(FormatXml, true)
-      .setPreference(IndentPackageBlocks, true)
-      .setPreference(AlignSingleLineCaseStatements, false)
-      .setPreference(IndentLocalDefs, false)
-      .setPreference(DanglingCloseParenthesis, Prevent)
-      .setPreference(SpaceInsideParentheses, false)
-      .setPreference(SpaceInsideBrackets, false)
-      .setPreference(SpacesWithinPatternBinders, true)
-      .setPreference(MultilineScaladocCommentsStartOnFirstLine, false)
-      .setPreference(IndentWithTabs, false)
-      .setPreference(CompactControlReadability, false)
       .setPreference(PlaceScaladocAsterisksBeneathSecondAsterisk, true)
-      .setPreference(SpacesAroundMultiImports, false)
+      .setPreference(PreserveSpaceBeforeArguments, false)
+      .setPreference(DanglingCloseParenthesis, Prevent)
+  }
 
   override def trigger = Plugins.allRequirements
   override def requires = GilescSbt
