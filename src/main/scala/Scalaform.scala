0de8b3aed8bb4c324d839c347b052a6742d3eb8e
Craig Giles <craig@gilesc.com>
2016-03-02 06:38:11 -0800

    Updated to be used in settings

--- src/main/scala/Scalaform.scala
+++ src/main/scala/Scalaform.scala
@@ -9,12 +9,11 @@ import com.typesafe.sbt.SbtScalariform.ScalariformKeys
 import scalariform.formatter.preferences._
 
 object Scalaform extends sbt.AutoPlugin {
-  lazy val settings = SbtScalariform.scalariformSettings ++ Seq(
+  lazy val settings =
     ScalariformKeys.preferences := ScalariformKeys.preferences.value
       .setPreference(DoubleIndentClassDeclaration, true)
       .setPreference(PlaceScaladocAsterisksBeneathSecondAsterisk, true)
       .setPreference(DanglingCloseParenthesis, Prevent)
-  )
 
   override def trigger = Plugins.allRequirements
   override def requires = GilescSbt
